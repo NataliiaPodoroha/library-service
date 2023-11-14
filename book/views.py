ec79b976e32bb16fd305cf64449f7671a0950437
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-14 16:03:13 +0100

    Add permissions to books service

--- book/views.py
+++ book/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
+
 from book.models import Book
 from book.serializers import BookSerializer
 
@@ -6,3 +8,8 @@ from book.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        return [IsAdminUser()]

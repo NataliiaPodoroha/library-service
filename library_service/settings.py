bf036491cff322ba104d049a2f2ddb27297ee2a9
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-14 15:26:56 +0100

    Add AUTH_USER_MODEL to settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -108,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+
+AUTH_USER_MODEL = "user.User"
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"

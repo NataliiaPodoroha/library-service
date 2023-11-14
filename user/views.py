a6a311cd41750d3b1b6557f9204a1ee5d6ac63c0
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-14 14:59:29 +0100

    Add views for user model

--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """View for creating a new user."""
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """View for retrieving and updating the authenticated user's details."""
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve the authenticated user."""
+        return self.request.user

6570b2091c30591133c6a90ec0589d8e476e3263
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-15 15:04:02 +0100

    Add urls for borrowing model and include it to project urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book/", include("book.urls", namespace="book")),
+    path("api/borrowing/", include("borrowing.urls", namespace="borrowing")),
     path("api/user/", include("user.urls", namespace="user")),
 ]

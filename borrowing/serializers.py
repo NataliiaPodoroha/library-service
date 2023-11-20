b4752d33dfdea192ae2dac5177f92ee3cfd6284d
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-20 21:46:07 +0100

    Update borrowing serializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -5,12 +5,6 @@ from borrowing.models import Borrowing
 from user.serializers import UserSerializer
 
 
-class BorrowingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowing
-        fields = "__all__"
-
-
 class BorrowingListSerializer(serializers.ModelSerializer):
     book_title = serializers.CharField(source="book.title", read_only=True)
     user_email = serializers.CharField(source="user.email", read_only=True)

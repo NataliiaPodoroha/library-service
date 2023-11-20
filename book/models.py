047f97732b7845f01a9cf4096ff489d518dae051
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-20 12:54:38 +0100

    Update book model

--- book/models.py
+++ book/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=64, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         ordering = ["title"]

57bedbcc5b9f9ee5757421a51426db4e4efa164f
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-20 12:42:05 +0100

    Update BorrowingViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -2,16 +2,27 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingListSerializer, BorrowingDetailSerializer, BorrowingSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer,
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+)
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingSerializer
     permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
         if self.action == "list":
             return BorrowingListSerializer
         if self.action == "retrieve":
             return BorrowingDetailSerializer
         return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

3d4a026e0cc1e682364e1f636f65c48fb6ff09e8
Nataliia Podoroha <ingenuous999@gmail.com>
2023-11-24 15:24:13 +0100

    Create a helper function, which will receive borrowing as a parameter, and create a new Stripe Session for it

--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,16 @@
+import stripe
+from django.conf import settings
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from book.models import Book
+from borrowing.models import Borrowing
 from payment.models import Payment
 from payment.serializers import PaymentDetailSerializer, PaymentSerializer
 
 
 class PaymentViewSet(
+    mixins.CreateModelMixin,
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet,
@@ -25,3 +30,30 @@ class PaymentViewSet(
         if self.action == "retrieve":
             return PaymentDetailSerializer
         return PaymentSerializer
+
+
+def create_checkout_session(domain_url: str, borrowing_id: int, money_amount: int):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    borrowing = Borrowing.objects.get(pk=borrowing_id)
+    book = Book.objects.get(pk=borrowing.book.id)
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + "success/",
+            cancel_url=domain_url + "cancelled/",
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[{
+                "price_data": {
+                    "currency": "usd",
+                    "unit_amount": money_amount,
+                    "product_data": {
+                        "name": book.title,
+                        "description": "Book borrowing",
+                    },
+                },
+                "quantity": 1,
+            }],
+        )
+        return {"session_id": checkout_session["id"], "session_url": checkout_session["url"]}
+    except Exception as e:
+        return {"error": str(e)}

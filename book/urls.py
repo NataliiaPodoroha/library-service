f75dea9af1849eafc5533ca0e4cd137734b34176
Nataliia Podoroha <ingenuous999@gmail.com>
2023-12-01 15:42:37 +0100

    Fix urls (without "path")

--- book/urls.py
+++ book/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework import routers
 
 from book.views import BookViewSet
@@ -6,6 +5,6 @@ from book.views import BookViewSet
 router = routers.DefaultRouter()
 router.register("", BookViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
 
 app_name = "book"
